4c4af22e4c371a957af281d4b3b3996e8241e15b
Piotr Pabiś <p.pabis89@gmail.com>
2025-02-26 00:58:14 +0100

    feat: more skeleton, added intefaces

---
+++ ShoppingCart.Application/Interfaces/IProducts.cs
@@ -0,0 +1,9 @@
+﻿using ShoppingCart.Application.Dto;
+
+namespace ShoppingCart.Application.Interfaces
+{
+    public interface IProducts
+    {
+        Task<IEnumerable<ProductDto>> GetAllProducts();
+    }
+}
---
+++ ShoppingCart.Infrastructure/Api/FakeApiClient.cs
@@ -0,0 +1,13 @@
+﻿using ShoppingCart.Application.Dto;
+using ShoppingCart.Application.Interfaces;
+
+namespace ShoppingCart.Infrastructure.Api
+{
+    public class FakeApiClient : IProducts
+    {
+        public Task<IEnumerable<ProductDto>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
