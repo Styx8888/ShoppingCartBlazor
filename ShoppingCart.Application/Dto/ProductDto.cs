5178d8e785060d1816980e8888b4ce8f3c0b82d3
Piotr Pabiś <p.pabis89@gmail.com>
2025-02-26 00:48:05 +0100

    feat: added needed projects, added domain and DTOs

---
+++ ShoppingCart.Application/Dto/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ShoppingCart.Application.Dto
+{
+    public record Rating(double Rate, int Count);
+
+    public record ProductDto(
+        int Id,
+        string Title,
+        double Price,
+        string Description,
+        string Category,
+        string Image,
+        Rating Rating);
+
+}
---
+++ ShoppingCart.Domain/ProductWithRecommendation.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoppingCart.Domain
+{
+    public record ProductWithRecommendation(
+        int Id,
+        string Title,
+        double Price,
+        string Description,
+        string Category,
+        string Image,
+        int Stars,
+        bool Recommendation);
+}
